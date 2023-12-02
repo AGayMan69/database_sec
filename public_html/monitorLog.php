f51dfdca5b1deab5c039ef10b944621092d7f55c
AGayMan69 <scjohnwoo123@gmail.com>
2023-12-02 08:40:52 +0800

    bro

--- public_html/monitorLog.php
+++ public_html/monitorLog.php
@@ -5,7 +5,7 @@
 function array_tail($array, $n) {
     return array_slice($array, -$n);
 }
-$file = '/var/log/mysql/mysql.log'; // replace with your log file path
+$file = '/var/log/mysql/mysql.log';
 $lines = array_tail(file($file), 100);
 foreach ($lines as $line) {
     echo $line . "<br />";
